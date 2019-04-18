10633287bb2f3f397014906f2bc8eb7f653a1af1
rahul <rahulmukundan999@gmail.com>
2019-04-18 19:30:53 +0530

    changes made

--- services/mongodb.js
+++ services/mongodb.js
@@ -10,66 +10,69 @@ function mongodb() {
 
 
     function connectDb(dbName, cb) {
-        var url = 'mongodb://localhost:27017';
+        // var url = 'mongodb://localhost:27017';
+        var url = 'mongodb://rahul:rahul1234@ds127731.mlab.com:27731/eventest'
         MongoClient.connect(url, {
                 reconnectTries: 5,
                 reconnectInterval: 1000 * 5
-        },
-        function (err, client) {
-            console.log(err);
-            if (!err) {
-                var db = client.db(dbName);
-                cb({
-                    status : 200,
-                    db : db
-                });
-            } else {
-                cb({
-                    status: 500,
-                    msg: "Unable to get db connection"
-                });
-            }
-        });
+            },
+            function (err, client) {
+                console.log(err);
+                if (!err) {
+                    var db = client.db(dbName);
+                    cb({
+                        status: 200,
+                        db: db
+                    });
+                } else {
+                    cb({
+                        status: 500,
+                        msg: "Unable to get db connection"
+                    });
+                }
+            });
     }
-    function getData(data,callback) {
-        connectDb(data.dbName , tempData =>{
-            if(tempData.status == 200) {
+
+    function getData(data, callback) {
+        connectDb(data.dbName, tempData => {
+            if (tempData.status == 200) {
                 var dataList = [];
                 var db = tempData.db;
-                if(!data.limit) {
+                if (!data.limit) {
                     data.limit = 0;
                 }
-                if(!data.skip) {
+                if (!data.skip) {
                     data.skip = 0;
                 }
-                if(!data.sort) {
+                if (!data.sort) {
                     data.sort = 0;
                 }
-                if(data.query) {
+                if (data.query) {
                     if (data.query.id) {
                         data.query._id = ObjectId(data.query.id);
                         delete data.query.id;
                     }
                 }
                 var cursor = db.collection(data.tableName).find(data.query, data.options).limit(data.limit).skip(data.skip).sort(data.sort);
-                    cursor.each(function (err, doc) {
-                        if (doc != null) {
-                            dataList.push(doc);
-                        } else {
-                            callback({
-                                status: 200,
-                                data: dataList
-                            });
-                        }
-                    });
+                cursor.each(function (err, doc) {
+                    if (doc != null) {
+                        dataList.push(doc);
+                    } else {
+                        callback({
+                            status: 200,
+                            data: dataList
+                        });
+                    }
+                });
             } else {
                 callback(tempData);
             }
         })
     }
+
     function insert(data, callback) {
         connectDb(data.dbName, tempData => {
-            if(tempData.status == 200) {
+            if (tempData.status == 200) {
                 var db = tempData.db;
                 db.collection(data.tableName).insertOne(data.value, function (err, result) {
                     if (err) {
